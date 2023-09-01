9dfaacc043c9ebd709e44af44ba6c95526d5c7c9
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-09-01 20:52:07 +0300

    Initial commit for Simple Login Screen project

---
+++ Projects/SimpleLoginScreen/SimpleLoginScreen/LoginView.swift
@@ -0,0 +1,30 @@
+//
+//  File name: LoginView.swift
+//  Project name: SimpleLoginScreen
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 01/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ Projects/SimpleLoginScreen/SimpleLoginScreen/SimpleLoginScreenApp.swift
@@ -0,0 +1,20 @@
+//
+//  File name: SimpleLoginScreenApp.swift
+//  Project name: SimpleLoginScreen
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 01/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+@main
+struct SimpleLoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
