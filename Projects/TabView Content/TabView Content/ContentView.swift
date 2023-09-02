734fdad1243f9febdc9e06e7e3ac9f1079d47706
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-09-02 17:40:02 +0300

    TabView Content Project: initial commit.

---
+++ Projects/TabView Content/TabView Content/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  File name: ContentView.swift
+//  Project name: TabView Content
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 02/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projects/TabView Content/TabView Content/TabView_ContentApp.swift
@@ -0,0 +1,20 @@
+//
+//  File name: TabView_ContentApp.swift
+//  Project name: TabView Content
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 02/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+@main
+struct TabView_ContentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
