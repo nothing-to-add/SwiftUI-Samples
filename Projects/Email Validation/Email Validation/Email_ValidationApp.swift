e6b2ec2871457091dd8e30abeeab21d41eeb1f55
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-09-04 23:34:36 +0300

    Email Validation Project: initial commit

---
+++ Projects/Email Validation/Email Validation/Email_ValidationApp.swift
@@ -0,0 +1,20 @@
+//
+//  File name: Email_ValidationApp.swift
+//  Project name: Email Validation
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 04/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+@main
+struct Email_ValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
