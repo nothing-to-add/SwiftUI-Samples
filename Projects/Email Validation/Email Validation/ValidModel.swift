cc3cd30d871c6a4db3e47ca1e563527c868e7f60
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-09-05 00:36:10 +0300

    Email Validation Project: implementation of comparing with data

---
+++ Projects/Email Validation/Email Validation/ValidModel.swift
@@ -0,0 +1,15 @@
+//
+//  File name: ValidModel.swift
+//  Project name: Email Validation
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 05/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import Foundation
+
+struct ValidModel {
+    let data: [String] = [ "example@example.com", "data@data.com", "point@point.com" ]
+}
