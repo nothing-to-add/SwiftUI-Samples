1f9cc307da8d0338c047ced833b5c61da526b71c
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-09-05 20:43:28 +0300

    Initial Screen Project: init commit

---
+++ Projects/Init Screen/Init Screen/Init_ScreenApp.swift
@@ -0,0 +1,20 @@
+//
+//  File name: Init_ScreenApp.swift
+//  Project name: Init Screen
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 05/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+@main
+struct Init_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
