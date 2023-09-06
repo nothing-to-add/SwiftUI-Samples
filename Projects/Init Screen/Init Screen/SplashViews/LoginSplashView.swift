10855e5aed62a8346b4dfd7b22c2c8675163437e
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-09-06 23:07:33 +0300

    Initial Screen Project: implementation of the Login Splash screen

---
+++ Projects/Init Screen/Init Screen/SplashViews/LoginSplashView.swift
@@ -0,0 +1,50 @@
+//
+//  File name: LoginSplashView.swift
+//  Project name: Init Screen
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 06/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+struct LoginSplashView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State var isActive: Bool = false
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Welcome to our App")
+                .font(.system(size: 36, design: .default))
+                .multilineTextAlignment(.center)
+                .bold()
+            Spacer()
+            if self.isActive {
+                Button("Login") {
+                    dismiss()
+                }
+                .font(.system(size: 36, design: .default))
+                .controlSize(.large)
+                .buttonStyle(.borderedProminent)
+                Spacer()
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+//struct LoginSplashView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        LoginSplashView()
+//    }
+//}
+
