62c06d3df41a4ab560b460033ac85d32298bf580
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-09-08 22:57:33 +0300

    Initial Screen Project: implementation of Opacity Splash screen

--- Projects/Init Screen/Init Screen/SplashViews/OpacitySplashView.swift
+++ Projects/Init Screen/Init Screen/SplashViews/OpacitySplashView.swift
@@ -8,4 +8,44 @@
 //  Copyright (c) 2023 nothing-to-add
 //
 
-import Foundation
+import SwiftUI
+
+struct OpacitySplashView: View {
+    @State private var textOpacity: Double = 0
+    
+    var body: some View {
+        ZStack {
+            Color(UIColor.lightGray)
+                .opacity(0.3)
+                .ignoresSafeArea()
+            GeometryReader { geo in
+                VStack {
+                    Image(systemName: "figure.mind.and.body")
+                        .font(.system(size: 100))
+                        .shadow(color: .gray, radius: 10, x: 0, y: 5)
+                        .frame(height: geo.size.height/2)
+                        .opacity(textOpacity)
+                    
+                    Text("Nothing-to-add")
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .fontWeight(.bold)
+                        .foregroundColor(.primary)
+                        .opacity(textOpacity)
+                        .frame(width: geo.size.width, height: geo.size.height/2, alignment: .center)
+                }
+            }
+            .onAppear {
+                withAnimation(.easeInOut(duration: 1.5).delay(0.5)) {
+                    textOpacity = 1.0
+                }
+            }
+        }
+    }
+}
+
+struct OpacitySplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        OpacitySplashView()
+    }
+}
