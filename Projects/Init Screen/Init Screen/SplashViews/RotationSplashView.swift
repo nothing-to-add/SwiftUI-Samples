e526ae539bbb87f6e2855296133f579c7f4f3793
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-09-08 23:00:25 +0300

    Initial Screen Project: init commit for Rotation Splash Screen

---
+++ Projects/Init Screen/Init Screen/SplashViews/RotationSplashView.swift
@@ -0,0 +1,11 @@
+//
+//  File name: RotationSplashView.swift
+//  Project name: Init Screen
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 08/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import Foundation
