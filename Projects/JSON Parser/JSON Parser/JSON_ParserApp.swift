e116c1fbefdf5409d9e5826eaa450ceb71d9fd1c
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-09-27 22:58:01 +0300

    JSON Parser Project: initial commit for Json parser project

---
+++ Projects/JSON Parser/JSON Parser/JSON_ParserApp.swift
@@ -0,0 +1,20 @@
+//
+//  File name: JSON_ParserApp.swift
+//  Project name: JSON Parser
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 27/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+@main
+struct JSON_ParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
