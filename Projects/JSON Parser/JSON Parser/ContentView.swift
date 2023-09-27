c2a3c0b56bd404a74e5215f70a7be51ce49a8650
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2023-09-27 22:58:01 +0300

    JSON Parser Project: initial commit for Json parser project

---
+++ Projects/JSON Parser/JSON Parser/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  File name: ContentView.swift
+//  Project name: JSON Parser
+//  Workspace name: SwiftUI-Samples
+//
+//  Created by: nothing-to-add on 27/09/2023
+//  Using Swift 5.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
